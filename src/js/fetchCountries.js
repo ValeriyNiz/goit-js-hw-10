03e915f17f945887ae718c5549a8117c170cd895
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-08-30 23:13:45 +0300

    task1

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,6 @@
+export const fetchCountries = (name) => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(resp => {
+      return resp.json();
+    });
+}
