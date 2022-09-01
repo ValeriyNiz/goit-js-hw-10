405f837cc2552ccc4bfceb58f34c1038d2452a4c
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-09-01 23:34:42 +0300

    HW №10 bugfix

--- src/index.js
+++ src/index.js
@@ -3,27 +3,64 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const DEBOUNCE_DELAY = 300;
-const searchInput = document.getElementById('search-box');
 const refs = {
-  countryInfo: document.querySelector('.country-info')
-}
+  countryInfo: document.querySelector('.country-info'),
+  countryList: document.querySelector('.country-list'),
+  searchInput: document.getElementById('search-box'),
+};
 
-searchInput.addEventListener('input', debounce(event => {
-  fetchCountries(event.target.value.trim())
+const DEBOUNCE_DELAY = 300;
+
+refs.searchInput.addEventListener('input', debounce(event => {
+  const value = event.target.value.trim();
+
+  if (!value) {
+    clearHtml();
+
+    return;
+  }
+
+  fetchCountries(value)
     .then(data => {
-      // if (data.status === 404) {
-      //   Notify.failure('Oops, there is no country with that name')
-      //   return
-      // }
-      // if (data.length > 10) {
-      //   Notify.info('Too many matches found. Please enter a more specific name.')
-      // }      
-    })      
+      if (data.status === 404 || data.length > 10) {
+        data.status === 404
+          ? Notify.failure('Oops, there is no country with that name')
+          : Notify.info('Too many matches found. Please enter a more specific name.');
+
+        return;
+      }
+
+      clearHtml();
+      data.length >= 2 ? renderCountryList(data) : renderCountry(data[0]);
+    });
 }, DEBOUNCE_DELAY));
 
+function clearHtml() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
 
+function renderCountry({ flags, name, capital, population, languages }) {
+  refs.countryInfo.innerHTML = (
+    `<div class="country-card">
+      <div class="country-header">
+        <img class="country-flag" src="${flags.svg}" alt="${name.official}">
+        <h2 class="name">${name.official}</h2>
+      </div>
+      <p class="country-info"><b>Capital:</b> ${capital}</p>
+      <p class="country-info"><b>Population:</b> ${population}</p>
+      <p class="country-info"><b>Languages:</b> ${Object.values(languages).join(', ')}</p>
+    </div>`
+  );
+}
 
-
-
-npm install --save-dev parcel-transformer-hbs
+function renderCountryList(countryList) {
+  refs.countryList.innerHTML = countryList.map(({ flags, name }) => {
+    return (
+      `<li class="country-item">
+        <img class="country-flag" src="${flags.svg}" alt="${name.official}">
+        <h2 class="name">${name.official}</h2>
+      </li>`
+    );
+  }).join('');
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,22 +1,6 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import countryInfo from '../templates/country-info.hbs'
-
 export const fetchCountries = (name) => {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(resp => {    
-      if (resp.status === 404) {
-        Notify.failure('Oops, there is no country with that name')
-        return
-      }      
+    .then(resp => {
       return resp.json();
-    })  
-    .then(name => {
-      if (name.length > 10) {
-        Notify.info('Too many matches found. Please enter a more specific name.')
-      }
-      console.log(name);
-      const markup = countryInfo(name)
-      console.log(markup);
-    })
-    
+    });
 }
