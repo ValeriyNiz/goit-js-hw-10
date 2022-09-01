16e0cb2dfffe2241f65b53ac6fc325434f812074
ValeriyNiz <nizucer123@gmail.com>
2022-09-01 15:32:36 +0300

    add Notify(failure and info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 const searchInput = document.getElementById('search-box');
 
 searchInput.addEventListener('input', debounce(event => {
-  fetchCountries(event.target.value.trim()).then(data => {
-    console.log(data)
-  });
+  fetchCountries(event.target.value.trim())
+    .then(data => {
+      if (data.status === 404) {
+        Notify.failure('Oops, there is no country with that name')
+        return
+      }
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.')
+      }
+      console.log(data)
+    })      
 }, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,7 @@
+
 export const fetchCountries = (name) => {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(resp => {
+    .then(resp => {        
       return resp.json();
-    });
+    })    
 }
